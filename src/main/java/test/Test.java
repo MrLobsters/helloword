2ae38b0fd78e9fd613c1e43bd72ba0193ee6b3c0
zhr <zhr@LAPTOP-N8PNN901>
2019-07-31 10:32:05 +0800

    test

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,28 @@
+/** 注意：本内容仅限于本项目，禁止外泄
+ * @title: Test.java   
+ * @package test   
+ * @description: TODO描述
+ * @author: zhr   
+ * @date: 2019年7月31日 上午10:31:11   
+ * @version by(V2.0)
+ * @copyright: 2019
+ */
+package test;
+
+
+/** 注意：本内容仅限于本项目，禁止外泄
+ *
+ * @ClassName: Test
+ * @description: TODO [类的作用描述]
+ * @author: zhr
+ * @date: 2019年7月31日 上午10:31:11
+ *
+ * @version by(V2.0)
+ * @copyright: 2019
+ */
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("测试上传!");
+	}
+}
